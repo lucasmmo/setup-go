d80d36a837c88b59f038d18f255c8a7e5589010b
lucasmmo <lucasmocerinomonteiro@gmail.com>
2021-12-06 14:03:37 -0300

    Update the name of packages

--- pkg/hello/hello_test.go
+++ pkg/hello/hello_test.go
@@ -1,14 +1,14 @@
-package services_test
+package hello_test
 
 import (
 	"testing"
 
-	"github.com/lucasmmo/setup-go/pkg/services"
+	"github.com/lucasmmo/setup-go/pkg/hello"
 )
 
 func TestSayHello(t *testing.T) {
 	t.Run("Should say hello world", func(t *testing.T) {
-		s := services.SayHello("")
+		s := hello.Say("")
 		if s != "Hello, World!" {
 			t.Errorf("expected: Hello, World!, received: %s", s)
 		}
